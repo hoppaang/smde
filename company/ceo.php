f63f613837cce35f99a9bbc1e85b035ca4ca6fa1
hoppaang <hoppaang@gmail.com>
2016-07-26 15:49:22 +0900

    ceo style

--- company/ceo.php
+++ company/ceo.php
@@ -10,8 +10,9 @@
 	<!-- submenu incloud --><?php virtual('../common/submenu.php');?>
 
 	<!-- subcontent begin -->
-	<div class="subcontent">
-		ceo
+	<div class="subcontent ceo">
+		SMDE는 새만금 SEA FRONT(해양레저관광 복합단지)사업의 특수목적법인입니다</br>
+		국내외 건설시공 및 분양 전문가로 구성되어 있으며 그간  다양한 분야에서의 기술개발 및 컨설팅을 바탕으로 성공적인 사업수행을 위한 Know-How 보유하고 대규모 시스템 구축사업 및 유지보수 등에서의 경험이 풍부한 건설시공, 분양, Service 전문인력 다수 확보하고 분야별 전문업체가 참여한 협업체계구성으로 시너지 효과창출 극대화(국외 인증기관 감리 참여)하고 다양한 공공프로젝트 수행을 통한 검증된 품질관리체계 보유 전자정부지원사업 수행 경험을 통한 완벽한 사업관리 수행을 목표로 지향하고 있습니다.
 	</div>
 	<!-- subcontent end -->
 </div>
