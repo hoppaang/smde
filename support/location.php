7c5c865dbee0e81a00f112f167b926fcfc9a5904
hoppaang <hoppaang@gmail.com>
2016-07-26 15:00:34 +0900

    location

--- support/location.php
+++ support/location.php
@@ -11,7 +11,29 @@
 
 	<!-- subcontent begin -->
 	<div class="subcontent">
-		오시는길
+		<!-- Google Map -->
+		<!-- <div id="map" class="map map-box map-box-space margin-bottom-40"></div> -->
+		<div class="map map-box margin-bottom-40">
+			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12919.881854364226!2d126.76235588345726!3d35.947692480566914!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x35705b59e0179c83%3A0x6e11d71919a9fb0!2s20-6+Heukseok-gil%2C+Gaejeong-myeon%2C+Gunsan%2C+Jeollabuk-do!5e0!3m2!1sen!2skr!4v1459958380167" width="100%" height="350" frameborder="0" style="border:0" allowfullscreen></iframe>
+		</div>
+		<!-- End Google Map -->
+
+		<!-- Contacts -->
+		<div class="headline"><h2>Contacts</h2></div>
+		<ul class="list-unstyled who margin-bottom-30">
+			<li><a href="#">전북 군산시 계정면 흑석길 20-6 (우:54175)</a></li>
+			<li><a href="#"><i class="fa fa-envelope"></i>info@sowp.com</a></li>
+			<li><a href="#"><i class="fa fa-phone"></i>063.461.5541</a></li>
+			<li><a href="#"><i class="fa fa-globe"></i>http://www.example.com</a></li>
+		</ul>
+
+		<!-- Business Hours -->
+		<div class="headline"><h2>Business Hours</h2></div>
+		<ul class="list-unstyled margin-bottom-30">
+			<li><strong>월~금요일:</strong> 9am ~ 6pm</li>
+			<li><strong>토요일:</strong> 9am ~ 1pm</li>
+			<li><strong>일요일/공휴일:</strong> 휴무</li>
+		</ul>
 	</div>
 	<!-- subcontent end -->
 </div>
