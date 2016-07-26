e1aae097506ff9a2a012028305857d0adf39cccb
hoppaang <hoppaang@gmail.com>
2016-07-26 16:30:15 +0900

    history style

--- company/history.php
+++ company/history.php
@@ -10,8 +10,19 @@
 	<!-- submenu incloud --><?php virtual('../common/submenu.php');?>
 
 	<!-- subcontent begin -->
-	<div class="subcontent">
-		history
+	<div class="subcontent history">
+		<div class="year y2014">
+			<ul>
+				<li>
+					<span class="monthday">10.23</span>
+					새만금 친환경 마리나/스마트 그리드 기반구축 투자협약(MOU)체결
+				</li>
+				<li>
+					<span class="monthday">2014.11 ~ 2015.11</span>
+					참여사 통합운영위원회 설립, 사업추진 회의 (총 7회)
+				</li>
+			</ul>
+		</div>
 	</div>
 	<!-- subcontent end -->
 </div>
