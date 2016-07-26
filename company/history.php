fe27ad2564a0bd68c54288def5409702c4b4645c
hoppaang <hoppaang@gmail.com>
2016-07-26 16:46:22 +0900

    history

--- company/history.php
+++ company/history.php
@@ -11,18 +11,74 @@
 
 	<!-- subcontent begin -->
 	<div class="subcontent history">
-		<div class="year y2014">
+		<!--2016 history begin -->
+		<div class="year y2015">
 			<ul>
 				<li>
-					<span class="monthday">10.23</span>
-					새만금 친환경 마리나/스마트 그리드 기반구축 투자협약(MOU)체결
+					<span class="monthday">04.</span>
+					투자유치국장 간담회 (새만금+기업7개 사)
 				</li>
+				<li>
+					<span class="monthday"></span>
+					인터파크 비즈마켓 참여 확약서 제출
+				</li>
+				<li>
+					<span class="monthday"></span>
+					조감도용역 계약/진행 중
+				</li>
+				<li>
+					<span class="monthday">02.</span>
+					사업시행자 지정신청, 컨소시엄 (SMDE)
+				</li>
+			</ul>
+		</div>
+		<!--2016 history end -->
+
+		<!--2015 history begin -->
+		<div class="year y2015">
+			<ul>
+				<li>
+					<span class="monthday">12.</span>
+					SPC(SMDE) 상세설계용역 계약 (청죽엔지니어링 [6.6억원, V.A.T 포함])
+				</li>
+				<li>
+					<span class="monthday">11.</span>
+					참여사 전체회의로 컨소시엄 구성 (SMDE)
+				</li>
+				<li>
+					<span class="monthday">11.10</span>
+					새만금 부안/군산 친환경 해양레저복합단지 Sea Front(마리나) 기반 구축 (MOU) 체결
+				</li>
+				<li>
+					<span class="monthday">05.</span>
+					기재부 실사단 마리나 부지 방문 (담당 국장 총 10명)
+				</li>
+				<li>
+					<span class="monthday">03.</span>
+					군산시 마리나 관련 산업부 보고
+				</li>
+				<li>
+					<span class="monthday">02.</span>
+					운영위원회 해상풍력 및 마리나 관련 새만금청 TF 보고
+				</li>
+			</ul>
+		</div>
+		<!--2015 history end -->
+
+		<!--2014 history begin -->
+		<div class="year y2014">
+			<ul>
 				<li>
 					<span class="monthday">2014.11 ~ 2015.11</span>
 					참여사 통합운영위원회 설립, 사업추진 회의 (총 7회)
 				</li>
+				<li>
+					<span class="monthday">10.23</span>
+					새만금 친환경 마리나/스마트 그리드 기반구축 투자협약(MOU)체결
+				</li>
 			</ul>
 		</div>
+		<!--2014 history end -->
 	</div>
 	<!-- subcontent end -->
 </div>
