2c259e16598d508999391e5f1d6520e7b205ce83
hoppaang <hoppaang@gmail.com>
2016-07-26 13:45:01 +0900

    company menu page

---
+++ company/partner.php
@@ -0,0 +1,21 @@
+<?php
+	$menucode = 'company';
+	$submenucode = 'partner';
+	require('../common/config.php') ;
+	virtual('../common/basetag.php');
+?>
+<!-- top color bar --><?php virtual('../common/colorbar.php');?>
+
+<div class="container">
+	<!-- submenu incloud --><?php virtual('../common/submenu.php');?>
+
+	<!-- subcontent begin -->
+	<div class="subcontent">
+		partner
+	</div>
+	<!-- subcontent end -->
+</div>
+
+
+<!---footer include-->
+<?php virtual('../common/footer.php');?>
